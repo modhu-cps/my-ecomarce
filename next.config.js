0185fd11614288d56d999d3f992efbf8caa87d31
Modhu Sudhan Roy <modhu.cyber@gmail.com>
2026-05-25 22:39:20 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',            // ← এটাই স্ট্যাটিক এক্সপোর্ট সক্রিয় করে
+  // output: 'export',  ← এই লাইনটা ডিলিট বা কমেন্ট করে দিন
   images: {
-    unoptimized: true,         // স্ট্যাটিক সাইটে ইমেজ অপটিমাইজেশন বন্ধ
+    unoptimized: true,
   },
 }
 
